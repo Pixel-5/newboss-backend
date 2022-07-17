df8c7a067531b4d2d5e021b506ab883279317e24
MoffatMore <mofenyimoffat@gmail.com>
2022-07-17 20:08:37 +0200

    initial commit

---
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
+
+class VerifyCsrfToken extends Middleware
+{
+    /**
+     * The URIs that should be excluded from CSRF verification.
+     *
+     * @var array
+     */
+    protected $except = [
+        'https://192.168.8.102:8000/api/broadcasting/auth/*',
+    ];
+}
